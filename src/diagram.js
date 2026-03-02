a5384cb1e56d949df676b7e63e8d6944eadba960
aliatwood <aliatwood0104@gmail.com>
2026-03-02 16:27:47 +0100

    Styleade till diagram sidan
    Jag la till options och la till fixerade storlekar i min diagram.scss partial

--- src/diagram.js
+++ src/diagram.js
@@ -1,6 +1,13 @@
 const button = document.getElementById("visakurserknapp");
 const sDiagram = document.getElementById("kursdiagram");
 
+const buttonProgram = document.getElementById("visaprogramknapp");
+const cDiagram = document.getElementById("programdiagram");
+
+let kursChart;
+let programChart;
+
+
 let adata = [];
 
 async function getData() {
@@ -11,7 +18,7 @@ async function getData() {
         }
         let data = await res.json();
         console.log(data);
-        adata = data.filter(item => item.type === "Kurs");
+        adata = data;
     }
     catch(error){
         console.log("something went wrong: " + error)
@@ -21,11 +28,13 @@ async function getData() {
 
     getData();
 
-
     button.addEventListener("click", () => {
         const top6 = adata.sort((a,b) => Number(b.applicantsTotal.trim()) - Number(a.applicantsTotal.trim())).slice(0,6);
 
-        new Chart(sDiagram, {
+        if (kursChart) {
+            kursChart.destroy();
+        }
+        kursChart = new Chart(sDiagram, {
             type: "bar",
             data: {
                 labels: top6.map(c => c.name),
@@ -33,6 +42,37 @@ async function getData() {
                     label: "Antal sökande:",
                     data: top6.map(c => Number(c.applicantsTotal.trim())),
                 }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        })
+    })
+
+    buttonProgram.addEventListener("click", () => {
+        const top5Program = adata.filter(item => item.type === "Program").sort((a,b) => Number(b.applicantsTotal.trim()) - Number(a.applicantsTotal.trim()))
+        .slice(0,5);
+
+        if (programChart) {
+            programChart.destroy();
+        }
+
+        programChart = new Chart(cDiagram, {
+            type: "pie",
+            data: {
+                labels: top5Program.map(p => p.name),
+                datasets: [{
+                    label: "Antal sökande",
+                    data: top5Program.map(p => Number(p.applicantsTotal.trim()))
+                }]
+            },
+            options: {
+                responsive: true,
             }
         })
     })
