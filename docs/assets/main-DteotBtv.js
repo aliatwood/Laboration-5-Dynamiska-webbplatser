aacba37cb3e0144a9be919c6f6f1d9e9e4fe06c7
aliatwood <aliatwood0104@gmail.com>
2026-03-09 15:13:35 +0100

    npm run build

--- docs/assets/main-DteotBtv.js
+++
@@ -1,1 +0,0 @@
-import"./styles-CUUM99vB.js";const n=document.querySelector("button");n.addEventListener("click",()=>{n.classList.add("spinning"),n.addEventListener("animationend",()=>{n.classList.remove("spinning")},{once:!0})});
